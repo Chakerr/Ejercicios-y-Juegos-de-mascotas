bdfc036511ecf426b2d530d3725256351313d732
Chakerr <luisgas012@gmail.com>
2025-03-11 23:06:48 -0500

    fix: mensaje repetido por consola

--- SpringServer/src/main/java/com/Mario/SpringServer/repository/Mascota/MedicamentoRepository.java
+++ SpringServer/src/main/java/com/Mario/SpringServer/repository/Mascota/MedicamentoRepository.java
@@ -6,5 +6,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface MedicamentoRepository extends JpaRepository<Medicamento, Long> {
-    List<Medicamento> findByAdministradoFalseAndProximaDosisBefore(LocalDateTime fecha);
+    // Buscar medicamentos no administrados cuya próxima dosis ya pasó
+    List<Medicamento> findByAdministradoFalseAndProximaDosisBefore(LocalDateTime ahora);
 }
